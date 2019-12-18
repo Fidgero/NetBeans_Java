05bed7418501ce6025638b277dc2d57ba8e19dc8
otmazka <svetlankas@hot.ee>
2019-12-18 13:30:34 +0200

    biblioteka

---
+++ src/entity/Book.java
@@ -0,0 +1,39 @@
+package entity;
+import java.io.Serializable;
+public class Book implements Serializable {
+    private String title;
+    private String author;
+    private int publishedYear;
+
+    public Book() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublishedYear() {
+        return publishedYear;
+    }
+
+    public void setPublishedYear(int publishedYear) {
+        this.publishedYear = publishedYear;
+    }
+    
+    public String toString() {
+        return "Book{" +"Title= " +title + ", Author= " +author + ", PubYear= " + publishedYear + "}";
+    }
+    
+}
